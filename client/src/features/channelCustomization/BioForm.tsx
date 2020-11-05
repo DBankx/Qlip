ec53436e6644c1b59d08afd3fcbcb36793a0b8ff
DBankx <61208493+DBankx@users.noreply.github.com>
2020-11-05 12:27:37 +0000

    finished design of ui for customizing channels

--- client/src/features/channelCustomization/BioForm.tsx
+++ client/src/features/channelCustomization/BioForm.tsx
@@ -5,34 +5,99 @@ import {Formik} from "formik";
 import {InputTextarea} from "primereact/inputtextarea";
 import {observer} from "mobx-react-lite";
 import {Button} from "primereact/button";
+import {InputText} from "primereact/inputtext";
+import {useMediaQuery} from "react-responsive";
 
 interface IProps{
     channel: IChannel
 }
 
 const BioForm : React.FC<IProps> = ({channel}) => {
+    const isMobile = useMediaQuery({query: "(max-width: 500px)"})
+    const isTablet = useMediaQuery({query: "(min-width: 500px) and (max-width: 1024px"});
     return (
-                   <Formik initialValues={{bio: channel.bio}} onSubmit={(values: IChannelFormValues) => console.log(values)}>
+                   <Formik initialValues={{bio: channel.bio ? channel.bio : "", twitter: channel.twitter ? channel.twitter : "", instagram: channel.instagram ? channel.instagram : "", twitch: channel.twitch ? channel.twitch : "", youtube: channel.youtube ? channel.youtube : ""}} onSubmit={(values: IChannelFormValues) => console.log(values)}>
                        {({
                            handleBlur,
                            handleSubmit,
                            values,
                            touched,
                            errors,
-                           handleChange
+                           handleChange,
+                           isSubmitting,
+                           dirty
                          }) => (
                             
-                           <form>
-                                   <div className={"p-grid"}>
+                           <form className={"bioform"}>
+                               <Card className={"card-border"}>
+                                   <div className={"p-grid bioform-grid"}>
                                        <p className={"p-col-1 hide-sm"}>Bio</p>
                                        <div className={"p-md-11 p-sm-11 p-lg-11 p-col-12"}>
-                               <InputTextarea value={values.bio} rows={4} autoResize className={"boxsizingBorder"} />
-                               <p style={{marginTop: "0.5em", fontSize: "0.9em"}}>
+                               <InputTextarea name={"bio"} onChange={handleChange} onBlur={handleBlur} value={values.bio} rows={4} autoResize className={"boxsizingBorder"} />
+                               <p style={{marginTop: "0.5em", fontSize: "0.9em", color: "#777777"}}>
                                    Who you are in fewer than 300 characters</p>
-
-                                           <Button style={{float: "right", marginTop: "0.5em"}} label={"Save changes"} className={"p-button-sm"} />
                                        </div>
                                    </div>
+                                   
+                                   <hr className={"divider p-mb-2 pmt-2 "}/>
+                                   
+                                   <div className={"p-grid bioform-grid p-ai-center"}>
+                                       <p className={"p-col-1 hide-sm"}>{isTablet ? <i className={"pi pi-twitter"} style={{fontSize: "1.2em"}} /> : "Twitter"}</p>
+                                       <div className={"p-md-11 p-sm-11 pg-11 p-col-12"}>
+                                           <span className={isMobile ? "p-input-icon-left" : ""} style={{width: "100%"}}>
+                                               {isMobile && <i className={"pi pi-twitter"} />}
+                                           <InputText style={{width: "100%"}} name={"twitter"} value={values.twitter} onChange={handleChange} onBlur={handleBlur} className={"boxsizingBorder"} placeholder={"Full twitter url link"} />
+                                           </span>
+                                           
+                                       </div>
+                                       </div>
+
+
+                                   <hr className={"divider p-mb-2 pmt-2"}/>
+
+                                   <div className={"p-grid bioform-grid p-ai-center"}>
+                                       <p className={"p-col-1 hide-sm"}>{isTablet ? <i className={"fab fa-twitch fa-6x"} style={{fontSize: "1.2em"}} /> : "Twitch"}</p>
+                                       <div className={"p-md-11 p-sm-11 pg-11 p-col-12"}>
+                                           <span className={isMobile ? "p-input-icon-left" : ""} style={{width: "100%"}}>
+                                               {isMobile && <i className={"fab fa-twitch"} />}
+                                               <InputText style={{width: "100%"}} name={"twitch"} value={values.twitch} onChange={handleChange} onBlur={handleBlur} className={"boxsizingBorder"} placeholder={"Full twitch url link"} />
+                                           </span>
+
+                                       </div>
+                                   </div>
+                                   
+                                   <hr className={"divider p-mb-2 pmt-2"}/>
+
+                                   <div className={"p-grid bioform-grid p-ai-center"}>
+                                       <p className={"p-col-1 hide-sm"}>{isTablet ? <i className={"fab fa-youtube fa-6x"} style={{fontSize: "1.2em"}} /> : "Youtube"}</p>
+                                       <div className={"p-md-11 p-sm-11 pg-11 p-col-12"}>
+                                           <span className={isMobile ? "p-input-icon-left" : ""} style={{width: "100%"}}>
+                                               {isMobile && <i className={"fab fa-youtube"} />}
+                                               <InputText style={{width: "100%"}} name={"youtube"} value={values.youtube} onChange={handleChange} onBlur={handleBlur} className={"boxsizingBorder"} placeholder={"Full youtube url link"} />
+                                           </span>
+
+                                       </div>
+                                   </div>
+
+                                   <hr className={"divider p-mb-2 pmt-2"}/>
+
+                                   <div className={"p-grid bioform-grid p-ai-center"}>
+                                       <p className={"p-col-1 hide-sm"}>{isTablet ? <i className={"fab fa-instagram fa-6x"} style={{fontSize: "1.2em"}} /> : "Instagram"}</p>
+                                       <div className={"p-md-11 p-sm-11 pg-11 p-col-12"}>
+                                           <span className={isMobile ? "p-input-icon-left" : ""} style={{width: "100%"}}>
+                                               {isMobile && <i className={"fab fa-instagram"} />}
+                                               <InputText style={{width: "100%"}} name={"instagram"} value={values.instagram} onChange={handleChange} onBlur={handleBlur} className={"boxsizingBorder"} placeholder={"Full instagram url link"} />
+                                           </span>
+
+                                       </div>
+                                   </div>
+
+                                   <hr className={"divider pmt-2"}/>
+                                   
+                                   <div style={{padding: "1em", width: "100%", background: "#121212"}}>
+                                   <Button type={"submit"}  disabled={isSubmitting || !dirty} icon={isSubmitting ? "pi pi-spin pi-spinner" : ""} label={"Save changes"} style={{fontWeight: 600, marginTop: "0 auto", width: isMobile ? "100%" : "auto"}} />
+                                   </div>
+                               </Card>
                            </form>
                        )}
                    </Formik>
